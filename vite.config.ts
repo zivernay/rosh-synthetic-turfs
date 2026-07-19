4b09fe9f95c197018aed3d141e29f67c0163f029
Doma Z.A <zivanai@debian.zad>
2026-07-19 03:32:41 +0200

    Change bilder to terser

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,22 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: './',
   plugins: [
     tailwindcss(),
   ],
-})
+  build: {
+    minify: 'terser', // Terser is better for mangling than esbuild
+    terserOptions: {
+      mangle: {
+        toplevel: true, // Renames top-level variables too
+        keep_fnames: false, // Ensures functions don't keep their names
+        keep_classnames: false,
+      },
+      compress: {
+        drop_console: true, // Removes console.logs for extra security
+        passes: 2, // Number of times to compress the code
+      },
+    },
+  },
+});
